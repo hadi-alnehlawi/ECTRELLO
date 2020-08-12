62dab60dd89d15ee6ea79a1f3cb6996e4ed0d53d
Hadi <hadi.alnehlawi@gmail.com>
2020-08-12 18:38:46 +0400

    update setup for pypi project

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
+    author="Hadi Alnehlawi",
+    author_email="nhadi82@hotmail.com",
+    description="A simple, productive and easy CLI to interact with trello API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hadi-alnehlawi/ECTRELLO",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
     name='ectrello',
-    version='0.1',
+    version='1.0.0',
     packages=find_packages(),
-    # py_modules=['main'],
     include_package_data=True,
     install_requires=[
         'Click', 'requests', 'autopep8', 'pycodestyle'
-
     ],
     entry_points={
         'console_scripts': [
